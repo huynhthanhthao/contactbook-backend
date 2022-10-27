8616ba4b45313614b7605830e487f28d5a790023
Thao <huynhthanhthaoctu@gmail.com>
2022-10-28 00:27:12 +0700

    Xu ly truy cap duong dan khong hop le

---
+++ src/components/Error.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="error flex-column text-center mt-5">
+        <img src="@/assets/error.png" alt="" style="width: 400px" />
+        <h1 class="text-light mt-2">Not Found!</h1>
+        <router-link to="/" class="btn btn-light">Back to home</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ErrorComponent",
+};
+</script>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,10 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../components/Home/Home";
 import ContactCreate from "../components/ContactCreate/ContactCreate.vue";
 import ContactUpdate from "../components/ContactUpdate/ContactUpdate.vue";
-
+import Error from "../components/Error.vue";
 const routes = [
     { path: "/", component: Home },
+    { path: "/:pathMatch(.*)*", component: Error },
     { path: "/create", component: ContactCreate },
     { path: "/update", component: ContactUpdate },
 ];
